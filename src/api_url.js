e50bb41e7c16ef45d18baba58f145e81499f43e3
Raman Madhukar <146058279+007raman@users.noreply.github.com>
2024-10-07 19:32:08 +0530

    solar

---
+++ src/api_url.js
@@ -0,0 +1,12 @@
+// const BASE_URL = "http://localhost:8000/api";
+// const BASE_URL = "https://sungrow-api.onrender.com/api";
+// const BASE_URL = "https://whale-app-rqcs6.ondigitalocean.app/api";
+const BASE_URL = "https://orca-app-kg2zb.ondigitalocean.app/api";
+// const BASE_URL = 'https://shark-app-t9hh3.ondigitalocean.app/api'
+export default BASE_URL;
+
+//https://stingray-app-myegm.ondigitalocean.app/api
+//http://64.227.128.113/api
+//https://sstonebats.com/api/
+//https://whale-app-rqcs6.ondigitalocean.app/api
+
---
+++ src/utility/OTP.js
@@ -0,0 +1,23 @@
+// // import client from 'twilio';
+// import {toast} from 'react-toastify';
+
+
+
+
+
+// const sendMessage = (message="This was raally good", receiver="+919140493078", otp="123456") => {
+
+//     client(accountSid, authToken).messages
+//         .create({
+//             body: message+otp,
+//             messagingServiceSid: 'MGb4a4fb92e8fc757e14262d8152b3402b',
+//             to: receiver
+//         })
+//         .then(message =>{ 
+//             console.log(message.sid);
+//             toast('OTP sent successfully');
+//         })
+//         .done();
+// }
+
+// export default sendMessage;
